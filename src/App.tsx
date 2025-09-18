cbcff3fd5ba27176863427b95357ae107f557757
mehdi95zeid <mehdi95zeid@gmail.com>
2025-09-18 11:27:52 +0330

    new file:   .prettierrc 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   public/Logo.svg 	modified:   src/App.tsx 	new file:   src/components/Layout.tsx 	new file:   src/components/Layout/Footer.tsx 	new file:   src/components/Layout/navBar.tsx 	new file:   src/components/home/Hero.tsx 	modified:   src/index.css 	modified:   src/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<div>Home Page</div>} />
+          <Route path='/shop' element={<div>Shop Page</div>} />
+          <Route path='/about' element={<div>About Us Page</div>} />
+          <Route path='/contact' element={<div>Contact Page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
