e05a1eaf4aea46343c54978c95c16b5f4dfc2862
mehdi95zeid <mehdi95zeid@gmail.com>
2025-09-21 15:21:51 +0330

    feat(lucide-react): update dependencies and add lucide-react dependency navbar made responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from 'react-router-dom'
+import { Menu, User } from 'lucide-react'
 import Navbar from './Layout/navBar'
 import Footer from './Layout/Footer'
 
@@ -6,8 +7,8 @@ export default function Layout() {
   return (
     <div className='flex flex-col min-h-screen m-auto'>
       <header>
-        <nav className='flex h-34 items-center justify-between max-w-[1440px] m-auto'>
-          <div className='flex items-center justify-center gap-2'>
+        <nav className='flex h-34 items-center justify-between max-w-[1440px] m-auto px-[5vw]'>
+          <div className='max-lg:order-2 flex items-center justify-center gap-2'>
             <img
               className=''
               src='./public/logo.svg'
@@ -17,16 +18,28 @@ export default function Layout() {
             />
             <h4 className='text-[28px] font-semibold'>WonderKids</h4>
           </div>
-          <Navbar />
-          <div>
-            <NavLink to='/'>Sign In</NavLink>
+          <div className='max-lg:hidden'>
+            <Navbar />
+          </div>
+          <div className='max-lg:order-1 lg:hidden'>
+            <Menu />
+          </div>
+          <div className='max-lg:order-3 flex h-full justify-center items-center gap-4'>
+            <NavLink className='body5 font-semibold' to='/'>
+              <span className='sm:hidden'>
+                <User />
+              </span>
+              <span className='max-sm:hidden'>Sign In</span>
+            </NavLink>
+            <button className='btn-outline body4 max-sm:hidden!'>
+              Contact Us
+              <img src='/Group 427319385.svg' alt='' className='h-10 w-10' />
+            </button>
           </div>
         </nav>
       </header>
       <main className='flex-grow'>
-        <div className='mx-auto'>
-          <Outlet />
-        </div>
+        <Outlet />
       </main>
       <Footer />
     </div>
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,56 +1,60 @@
 export default function Hero() {
   return (
-    <div className='relative flex flex-col items-center gap-12 h-[688px] max-w-[1440px] text-center mx-auto py-15'>
+    <div className='relative flex flex-col justify-between items-center gap-12 h-120 sm:h-140 lg:h-[80vh] max-w-[1440px] text-center mx-auto py-15 px-5 overflow-x-clip'>
       <h1 className='h1 font-semibold max-w-[937px] mx-auto'>
-        The best place to{' '}
+        The best place to <br />
         <span className='relative h2 font-normal font-courgette text-purple-300'>
           learn
           <img
-            className='absolute -z-10 -bottom-4 right-7'
+            className='absolute -z-10 -bottom-2 right-6 sm:-bottom-4 sm:right-7'
             src='/Line element 1.svg'
           ></img>
         </span>{' '}
-        and{' '}
+        and
         <span className='relative h2 font-normal font-courgette text-yellow-300'>
           play
           <img
-            className='absolute -z-10 -bottom-5 left-15'
+            className='absolute -z-10 -bottom-2 left-4 sm:-bottom-5 sm:left-15'
             src='/Line element 2.svg'
           ></img>
         </span>{' '}
         <br /> for kids
       </h1>
-      <p className='body4 text-gray dark:text-gray-400'>
+      <p className='max-sm:hidden body4 text-gray dark:text-gray-400'>
         Discover thousands of fun and interactive learning activities <br /> to
         support your child's growth and learning process.
       </p>
-      <button className='text-body4 flex justify-center items-center gap-4 font-semibold text-background dark:text-foreground rounded-[40px] bg-purple-300 h-18 w-[207px] hover:bg-purple-300/80 transition-all duration-1000 cursor-pointer'>
+      <p className='sm:hidden body4 text-gray dark:text-gray-400 max-w-100'>
+        Discover thousands of fun and interactive learning activities to support
+        your child's growth and learning process.
+      </p>
+      <button className='btn body4 w-[207px]'>
         Get Started
         <img className='' src='/Group 427319395.svg'></img>
       </button>
       <img
-        className='absolute -z-10 top-[42px] left-[77px]'
+        className='absolute -z-10 -top-[20px] left-[0px] lg:top-[42px] lg:left-[77px] max-sm:size-20'
         src='/Element 6.svg'
       ></img>
       <img
-        className='absolute -z-10 top-[228px] left-[142px]'
+        className='absolute -z-10 top-[70px] left-[10px] lg:top-[228px] lg:left-[142px] max-sm:size-14'
         src='/Element 7.svg'
       ></img>
       <img
-        className='absolute -z-10 bottom-[127px] left-[226px]'
+        className='absolute -z-10 bottom-[0px] -left-[40px] lg:bottom-[127px] lg:left-[226px]'
         src='/Element 8.svg'
       ></img>
       <img
-        className='absolute -z-10 bottom-[104px] right-[190px]'
+        className='absolute -z-10 bottom-[230px] -right-[50px] lg:bottom-[104px] lg:right-[190px] max-sm:size-40'
         src='/Element 4.svg'
         alt=''
       ></img>
       <img
-        className='absolute -z-10 top-[92px] right-[40px]'
+        className='absolute -z-10 -top-[40px] -right-[10px] lg:top-[92px] lg:right-[40px] max-sm:size-30'
         src='/Group 427319413.svg'
       ></img>
       <img
-        className='absolute -z-10 top-[315px] right-[155px]'
+        className='absolute -z-10 bottom-[150px] right-[20px] lg:top-[315px] lg:right-[155px]'
         src='/Element 5.svg'
       ></img>
     </div>
