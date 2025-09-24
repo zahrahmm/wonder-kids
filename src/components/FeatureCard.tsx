663943bf6e95504f0a858b7e4cb4317b85b34e84
Zahra Homami <zahra28582@gmail.com>
2025-09-24 16:45:14 +0330

    refactor: normalize line-height percentages for better responsive typography

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -2,7 +2,7 @@ export default function FeatureCard() {
   return (
     <div className='bg-gray-100 flex flex-col justify-center items-start gap-6 rounded-[48px] flex-1 h-[448px] w-[405px] transform transition duration-300 hover:scale-105'>
       <div className='flex flex-col justify-center items-start gap-4 px-8'>
-        <h2 className='body2 font-semibold'></h2>
+        <h2 className='h7 font-semibold'></h2>
         <p className='body5 font-medium text-gray'></p>
       </div>
     </div>
